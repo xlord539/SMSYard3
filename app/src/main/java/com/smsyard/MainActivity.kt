19f6a9a3dcc8c2a8432d281d009d6d464725f699
xlord539 <ahmedlord539@gmail.com>
2025-06-20 12:58:34 +0300

    Add files via upload

---
+++ app/src/main/java/com/smsyard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smsyard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // No setContentView for simplicity
+    }
+}
---
+++ gradlew
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+DIR=$(dirname "$0");
+exec gradle "$@"
